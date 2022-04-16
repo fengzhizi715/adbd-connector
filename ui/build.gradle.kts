0d2263f9d9a2dc60dbe314ba913f74f66cd333d7
Tony Shen <fengzhizi715@126.com>
2022-04-16 15:26:55 +0800

    update

---
+++ ui/build.gradle.kts
@@ -0,0 +1,31 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("java")
+    id("kotlin")
+    id("org.jetbrains.compose") version ("1.0.0")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    jcenter()
+    mavenCentral()
+    maven("https://jitpack.io")
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation(compose.desktop.currentOs)
+    implementation(compose.desktop.common)
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${rootProject.extra["kotlin_version"]}")
+
+    implementation(project(":core"))
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
---
+++ ui/src/main/kotlin/cn/netdiscovery/adbd/ui/launch.kt
@@ -0,0 +1,49 @@
+package cn.netdiscovery.adbd.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.lightColors
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.ApplicationScope
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+import kotlinx.coroutines.runBlocking
+
+/**
+ *
+ * @FileName:
+ *          cn.netdiscovery.adbd.ui.launch
+ * @author: Tony Shen
+ * @date: 2022/4/16 3:11 下午
+ * @version: V1.0 <描述当前版本功能>
+ */
+val padding = 13.dp
+val fontSize = 13.sp
+const val previewWidth = 300
+
+fun main() = application {
+
+    Window(
+//        icon = painterResource("image/ic_logo.ico"),
+        onCloseRequest = { closeRequest() },
+        title = "adbd-connector",
+        resizable = false,
+        state = rememberWindowState(width = Dp(previewWidth * 2.toFloat()), height = 400.dp)
+    ) {
+        MaterialTheme(colors = lightColors(primary = Color(0xFF999999))) {
+            Column(Modifier.background(MaterialTheme.colors.surface).padding(padding)) {
+            }
+        }
+    }
+}
+
+fun ApplicationScope.closeRequest() = runBlocking {
+    exitApplication()
+}
